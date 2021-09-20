d28698f9e62000236fde1f4425fd83986e407a1b
Mehmet Yasin Şeremet <myasinseremet@gmail.com>
2021-09-20 16:06:10 +0300

    first commit

---
+++ interSpective/src/test/java/com/example/interSpective/InterSpectiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interSpective;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterSpectiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
